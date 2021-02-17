f689aacc95a4f69c8b0f97c1cbac24b40714b5ae
Cuatroa <adrianandres1992@gmail.com>
2021-02-17 17:38:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import dotenv from  'dotenv'
 
 function App() {
+  const [image, setImage] = useState()
+
+  const handleChange = () => {
+    axios.get(`https://api.generated.photos/api/v1/faces?api_key=${process.env.REACT_APP_PHOTO_API_KEY}&order_by=random`).then(res => {
+      const uri = res.data.faces[0].urls[4][512];
+      uri && setImage(uri);
+    }).catch(err => {
+      console.log(err.message);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This person does not exist</h1>
+      {image && <img src={image} alt='AI Face' />}
+      <button type='button' onClick={handleChange}>
+      New face
+      </button>
     </div>
   );
 }
